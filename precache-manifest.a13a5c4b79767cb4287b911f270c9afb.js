85211658f22319c123026f7564e6c68be77074a8
Mattamorphic <9885259+Mattamorphic@users.noreply.github.com>
2019-08-31 20:25:35 +0100

    Updates

---
+++ precache-manifest.a13a5c4b79767cb4287b911f270c9afb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e96de619a7341df933972f2e4e58a3c",
+    "url": "/curlify/index.html"
+  },
+  {
+    "revision": "8110993491f82369ed6f",
+    "url": "/curlify/static/css/main.920f6212.chunk.css"
+  },
+  {
+    "revision": "929d2e5a50b988269a5c",
+    "url": "/curlify/static/js/2.69c3c128.chunk.js"
+  },
+  {
+    "revision": "8110993491f82369ed6f",
+    "url": "/curlify/static/js/main.22b1e5f4.chunk.js"
+  },
+  {
+    "revision": "b07c659718a5addbef17",
+    "url": "/curlify/static/js/runtime~main.f9da56b4.js"
+  }
+]);
