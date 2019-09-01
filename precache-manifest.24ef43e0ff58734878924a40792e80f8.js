09ab1611d3fb7ccdc0aaadc88ea8775a7fb99fb6
Mattamorphic <9885259+Mattamorphic@users.noreply.github.com>
2019-09-01 10:06:08 +0100

    Updates

--- precache-manifest.24ef43e0ff58734878924a40792e80f8.js
+++ precache-manifest.24ef43e0ff58734878924a40792e80f8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e96de619a7341df933972f2e4e58a3c",
+    "revision": "b0820e28e25f5d98431a1d0aab87295c",
     "url": "/curlify/index.html"
   },
   {
-    "revision": "8110993491f82369ed6f",
+    "revision": "7d6b7833433592e051fd",
     "url": "/curlify/static/css/main.920f6212.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/curlify/static/js/2.69c3c128.chunk.js"
   },
   {
-    "revision": "8110993491f82369ed6f",
-    "url": "/curlify/static/js/main.22b1e5f4.chunk.js"
+    "revision": "7d6b7833433592e051fd",
+    "url": "/curlify/static/js/main.fcdfc40f.chunk.js"
   },
   {
     "revision": "b07c659718a5addbef17",
