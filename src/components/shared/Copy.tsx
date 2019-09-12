c4b0dbf9841d9166745470bc962e194d50c458d1
Mattamorphic <9885259+Mattamorphic@users.noreply.github.com>
2019-09-12 10:45:08 +0100

    Fixed ListHistory components

--- src/components/shared/Copy.tsx
+++ src/components/shared/Copy.tsx
@@ -6,24 +6,36 @@ import Tooltip from './Tooltip';
 
 interface CopyProps {
   className?: string;
-  label?: string;
+  noIcon?: boolean;
+  useDiv?: boolean;
   content: string;
+  tooltip?: string;
 }
 
 const Copy: React.FunctionComponent<CopyProps> = props => {
   const copy = async () => {
     await navigator.clipboard.writeText(props.content);
   };
-
+  if (!props.useDiv) {
+    return (
+      <Tooltip text={props.tooltip || 'Copy'}>
+        <Button
+          className={props.className || ''}
+          isPrimary={false}
+          onClick={copy}
+        >
+          {!props.noIcon && <FontAwesomeIcon icon={faCopy} size="lg" />}
+          {props.children}
+        </Button>
+      </Tooltip>
+    );
+  }
   return (
-    <Tooltip text="Copy">
-      <Button
-        className={props.className || ''}
-        isPrimary={false}
-        onClick={copy}
-      >
-        <FontAwesomeIcon icon={faCopy} size="lg" />
-      </Button>
+    <Tooltip text={props.tooltip || 'Copy'}>
+      <div className={props.className || ''} onClick={copy}>
+        {!props.noIcon && <FontAwesomeIcon icon={faCopy} size="lg" />}
+        {props.children}
+      </div>
     </Tooltip>
   );
 };
